0e56d3287c49ff559975a950155fab9823e3d1f9
Kristina Epifanova <cristinaepifanova@gmail.com>
2022-10-20 18:58:51 +0300

    bugfix: card routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,9 @@
 const routerCards = require('express').Router();
 const {
-  getAllCards, getCardById, createCard, removeCardById, likeCardById, unlikeCardById,
+  getAllCards, createCard, removeCardById, likeCardById, unlikeCardById,
 } = require('../controllers/cards');
 
 routerCards.get('/cards', getAllCards);
-
-routerCards.get('/cards/:cardId', getCardById);
 
 routerCards.post('/cards', createCard);
 
